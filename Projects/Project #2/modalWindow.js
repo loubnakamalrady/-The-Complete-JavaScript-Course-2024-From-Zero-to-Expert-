17255ff6b6200aa06c41ac91e54c7d6dee516595
Loubna Kamal <loubnakamaly1998@gmail.com>
2024-01-03 00:11:53 +0200

    first commit

---
+++ Projects/Project #2/modalWindow.js
@@ -0,0 +1,5 @@
+const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('.close-modal');
+const overLay = document.querySelector('.overlay');
+const openModal = document.querySelectorAll('.show-modal');
+
