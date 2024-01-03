ccb390e3c668f7ee16462082039212a9a85d38ce
Loubna Kamal <loubnakamaly1998@gmail.com>
2024-01-03 13:47:10 +0200

    final commit

--- Projects/Project #2/modalWindow.js
+++ Projects/Project #2/modalWindow.js
@@ -1,5 +1,30 @@
 const modal = document.querySelector('.modal');
-const closeModal = document.querySelector('.close-modal');
+const btnCloseModal = document.querySelector('.close-modal');
 const overLay = document.querySelector('.overlay');
-const openModal = document.querySelectorAll('.show-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
 
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overLay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overLay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overLay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      closeModal();
+      console.log(e);
+    }
+  }
+});
